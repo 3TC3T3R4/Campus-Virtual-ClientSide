39a969d56d58e303ce8a663c1d9a1553f46e265b
ESTEVAN TANGARIFE CORREA <teban1928@gmail.com>
2023-04-25 20:03:46 -0500

    Necesary Files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
+import * as tslib from 'tslib';
 // import {
 //   AngularFireAuthGuard,
 //   redirectLoggedInTo,
--- src/bussiness/useCases/learningpath/create-learningpath.usecase.ts
+++ src/bussiness/useCases/learningpath/create-learningpath.usecase.ts
@@ -2,11 +2,12 @@ import { Observable } from 'rxjs';
 import { UseCase } from 'src/base/utils/IUseCase';
 import { NewLearningPathCommand } from 'src/domain/commands/learningpath/newLearningPathCommands';
 import { LearningPathRepository } from 'src/bussiness/repositories/learningpath/learningpath.repository';
+import { LearningPathModel } from 'src/domain/models/learningpath/learningpath';
 
-export class CreateLearningPathUseCase implements UseCase<NewLearningPathCommand, string> {
+export class CreateLearningPathUseCase implements UseCase<NewLearningPathCommand, LearningPathModel> {
     constructor(private learningPathRepository: LearningPathRepository) {}
 
-    execute(command: NewLearningPathCommand): Observable<string> {
+    execute(command: NewLearningPathCommand): Observable<LearningPathModel> {
     
         return this.learningPathRepository.createLearningPathAsync(command);
     
