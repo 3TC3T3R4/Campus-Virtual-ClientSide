3d9dd6ea57a8c7a036faf013abf4720f9e1a5f58
jmgaviriam <jmgaviriam@gmail.com>
2023-04-27 13:57:26 -0500

    Updating branch :)

--- src/presentation/modules/learningpath/pages/main-learningpaths/main-learningpaths.component.ts
+++ src/presentation/modules/learningpath/pages/main-learningpaths/main-learningpaths.component.ts
@@ -6,62 +6,45 @@ import { CreateLearningPathUseCase } from 'src/bussiness/useCases/learningpath/c
 @Component({
   selector: 'sofka-main-learningpaths',
   templateUrl: './main-learningpaths.component.html',
-  styleUrls: ['./main-learningpaths.component.scss']
+  styleUrls: ['./main-learningpaths.component.scss'],
 })
 export class MainLearningpathsComponent {
- //routes
- routeDashboard: string[];
- //variables
- render!: boolean;
- frmFormReactive : FormGroup;
+  //routes
+  routeDashboard: string[];
+  //variables
+  render!: boolean;
+  frmFormReactive: FormGroup;
 
- constructor(private taskCreate: CreateLearningPathUseCase, private router: Router) {
-   this.routeDashboard = ['../'];
-   this.render = true;
-   this.frmFormReactive = new FormGroup({
+  constructor(
+    private taskCreate: CreateLearningPathUseCase,
+    private router: Router
+  ) {
+    this.routeDashboard = ['../'];
+    this.render = true;
+    this.frmFormReactive = new FormGroup({
+      coachID: new FormControl('', [
+        Validators.required,
+        Validators.minLength(1),
+      ]),
+      title: new FormControl('', [
+        Validators.required,
+        Validators.minLength(1),
+      ]),
 
-    coachID: new FormControl('', [
-      Validators.required,
-      Validators.minLength(1),
-    ]),
-    title:new FormControl('',  [
-      Validators.required,
-      Validators.minLength(1)
-
-    ]),
-
-    description:new FormControl('',  [
-      Validators.required,
-      Validators.min(1)
-    ]),
-
-  });
-
-
-
-
- }
-
- sendData(): void {
-
-  if (this.frmFormReactive.invalid) {
-
-    alert('No puedes crear un articulo vacio, llenar todos los campos');
-
+      description: new FormControl('', [
+        Validators.required,
+        Validators.min(1),
+      ]),
+    });
   }
 
+  sendData(): void {
+    if (this.frmFormReactive.invalid) {
+      alert('No puedes crear un articulo vacio, llenar todos los campos');
+    }
 
-  console.log('sendData',this.frmFormReactive);
-  
-  this.taskCreate.execute(this.frmFormReactive.getRawValue()).subscribe({
-   
-  });
+    console.log('sendData', this.frmFormReactive);
 
-
-
-}
-
-
-
-
+    this.taskCreate.execute(this.frmFormReactive.getRawValue()).subscribe({});
+  }
 }
