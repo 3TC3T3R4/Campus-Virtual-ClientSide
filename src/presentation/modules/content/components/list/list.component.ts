b2222a771683535a0ef2bb73aad61a19eaa5565b
JuanJaramilloSalazar <salazarjdj@gmail.com>
2023-04-28 05:58:20 -0500

    Pagination list courses

--- src/presentation/modules/content/components/list/list.component.ts
+++ src/presentation/modules/content/components/list/list.component.ts
@@ -13,6 +13,14 @@ export class ListComponent {
 
   @Input() contents: ContentModel[];
 
+
+   //pagination
+   registrationsPerPageTable: number = 6;
+   page: number = 1;
+   pages: number[] = [];
+   totalPages: number = 0;
+
+
   constructor(private router: Router,
               private deleteContent: DeleteContentUseCase,
               private toastr: ToastrService){
@@ -47,4 +55,28 @@ export class ListComponent {
       complete: () => {console.log('Complete'), this.contents.splice(index, 1);}
     });
   }
+
+
+  //#region util methods
+  calculatePages(): void {
+    this.totalPages = Math.ceil(
+      this.contents.length / this.registrationsPerPageTable
+    );
+    this.pages = Array(this.totalPages)
+      .fill(0)
+      .map((x, i) => i + 1);
+  }
+
+  /* searchByType(term: string): void {
+    this.searching = true;
+    this.filteredRegistrations = this.registrationsList.filter(
+      (registrer) =>
+        registrer.description.toLowerCase().includes(term.toLowerCase())
+      // registrer.title.toLowerCase().includes(term.toLowerCase())
+    );
+  } */
+  //#endregion
+
+
+
 }
--- src/presentation/modules/course/components/list/list.component.ts
+++ src/presentation/modules/course/components/list/list.component.ts
@@ -13,6 +13,14 @@ export class ListComponent {
   
   @Input() courses: CourseModel[];
 
+
+   //pagination
+   contentsPerPageTable: number = 6;
+   page: number = 1;
+   pages: number[] = [];
+   totalPages: number = 0;
+
+
   constructor(private router: Router,
               private deleteCourse: DeleteCourseProfileUseCase,
               private toastr: ToastrService){
@@ -52,4 +60,29 @@ export class ListComponent {
     });
 
   }
+
+
+   //#region util methods
+   calculatePages(): void {
+    this.totalPages = Math.ceil(
+      this.courses.length / this.contentsPerPageTable
+    );
+    this.pages = Array(this.totalPages)
+      .fill(0)
+      .map((x, i) => i + 1);
+  }
+
+  /* searchByType(term: string): void {
+    this.searching = true;
+    this.filteredRegistrations = this.registrationsList.filter(
+      (registrer) =>
+        registrer.description.toLowerCase().includes(term.toLowerCase())
+      // registrer.title.toLowerCase().includes(term.toLowerCase())
+    );
+  } */
+  //#endregion
+
+
+
+
 }
